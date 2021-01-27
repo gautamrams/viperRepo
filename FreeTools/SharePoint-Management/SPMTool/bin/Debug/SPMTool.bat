20a7e5c8ec18171add2009272e01c3a3bae40b45
gautamrams <30833582+gautamrams@users.noreply.github.com>
2021-01-26 19:14:26 -0600

    e	deleted:    FreeTools/ADQuery/NewText/AttributeDlg.h

--- FreeTools/SharePoint-Management/SPMTool/bin/Debug/SPMTool.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-FOR /F "tokens=*" %%R IN ('powershell.exe Get-ExecutionPolicy') DO SET VAR=%%R
-echo "%var%"
-
-IF not %var% == RemoteSigned powershell.exe Set-ExecutionPolicy RemoteSigned
-
-powershell.exe -command .\SPMTool.ps1 SPMTool.dll
-exit
--- FreeTools/StartTools/mfc-start/mfc-startAD360/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// mfc-start.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
